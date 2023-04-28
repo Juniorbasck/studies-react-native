2da8560edbb76b000c93e84fccd3ee2c7894da7e
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-04-28 12:03:29 +0100

    atualizacoes

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
 import React from 'react';
 import Cesta from "./src/telas/Cesta"
+import {useFonts, Montserrat_400Regular, Montserrat_700Bold} from '@expo-google-fonts/montserrat';
 
 
 
 export default function App() {
+
+
+
+
   return (
     <View>
       <StatusBar />
